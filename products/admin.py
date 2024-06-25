32b4c4053eacda16c5c51856464142b9a898fb3a
broccol1l <xamrakulovramazan@gmail.com>
2024-06-25 19:07:04 +0500

    second_django

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import CategoryModel
 
-admin.site.register(CategoryModel)
+from .models import CategoryModel, ProductModel
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    search_fields = ['category_name']
+    list_display = ['id', 'category_name', 'created_at']
+    list_filter = ['created_at']
+    ordering = ['-id']
+
+@admin.register(ProductModel)
+class ProductModelAdmin(admin.ModelAdmin):
+    search_fields = ['product_name']
+    list_display = ['id', 'product_name', 'price', 'count', 'created_at']
+    list_filter = ['created_at']
+    ordering = ['-id']
+#admin.site.register(CategoryModel)
--- products/models.py
+++ products/models.py
@@ -3,3 +3,27 @@ from django.db import models
 
 class CategoryModel(models.Model):
     category_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class ProductModel(models.Model):
+    product_name = models.CharField(max_length=80)
+    price = models.FloatField()
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, null=True)
+    count = models.IntegerField(default=0)
+    description = models.TextField()
+    image = models.FileField(upload_to='product_image')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
